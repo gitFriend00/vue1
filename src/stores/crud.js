70ddcd2fdcfd367f2bf45bb10d85ee1cb65ec554
gitFriend00 <noble.history2019@gmail.com>
2026-04-26 21:54:02 -0800

    new: renamed store to crud for clean up

--- src/stores/crud.js
+++ src/stores/crud.js
@@ -1,7 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
+export const useCrudStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
   const nextId = ref(1)
